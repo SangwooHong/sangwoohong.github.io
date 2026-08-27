2d61dcd5bd8041edecd9caa0e3d1e14f0b0863b1
Sangwoo Hong <swhong06@konkuk.ac.kr>
2026-08-27 19:19:15 +0900

    Add member photo URLs

--- src/data/people.ts
+++ src/data/people.ts
@@ -13,45 +13,53 @@ export const people = {
     {
       name: "Hyungjoo Ahn",
       role: "Lab Leader / Master's Program",
-      interests: "Autonomous Driving"
+      interests: "Autonomous Driving",
+      image: "https://lh3.googleusercontent.com/sitesv/AG8ngQXzqaopJLi-NTXX8n3-BPkDwx0EGPvQ_CtSotm5U8S_QsAM1UO-RCfQiJTUOH05r60dbJCSYI04ncWZgc9bakcEvK78a0IBCrHctJAlS11930kTifBqTsDiUdBhwe5N2tXkVFVzB_hVscYLmV4NibSc9bEpDVCe0RTTGSQMqgptrXZNvzNpMjKZc8Rr6u8uiY4_FtvVneQ7O16cX5DbvhDlWqLJjrk1qx1LzN9kk3M=w1280"
     },
     {
       name: "Jinyoung Kim",
       role: "Integrated M.S.-Ph.D. Program",
       interests: "Machine Unlearning, Model Collapse",
-      github: "https://github.com/jinzer0"
+      github: "https://github.com/jinzer0",
+      image: "https://lh3.googleusercontent.com/sitesv/AG8ngQW6oCO_ypHboc_aAP7GAnyxOos7JZJmN9X6QDAM0A_Cq0zaPLZFjlzh-Ge1YYEArYRHvZtWVJA76nyJ4MLvjePom3l0wy2OTkB83sbE1GC0wfz611fyk9EiqmaC9sy0fSziQ03njqz3oBmlmbsfV7jHLE3jfRwlGFhE6uHrN2PqQYtxT1OhNO2v4w0JJY4Z1S_Rhyl4tV8ZV30Cf5jI4ho5sgZDrU0KUTuXKMdL=w1280"
     },
     {
       name: "Youngmin Oh",
       role: "Integrated M.S.-Ph.D. Program",
-      interests: "Machine Unlearning, Model Collapse"
+      interests: "Machine Unlearning, Model Collapse",
+      image: "https://lh3.googleusercontent.com/sitesv/AG8ngQUwP5R63vbKP3ezOvzi6lHYCKl9t8aUQk2c36laOl1JViGLSQbv2mBm0k3dEWn_TDhFRZz4fPqJCrymJIMIiVLMgrnl4rWk_M9CN0XnVRX9Ewp-ejZ9amjtnuDQB7oRkD5gGfsRM631DlH2D9iQx4cjUzdykZL7QCLSmOoZuod0jIBZuct0oQ69sIlFhtInBObuNKFJ9JYrtcPIIEElQt4m_osTdPni97yOTZeM=w1280"
     },
     {
       name: "Donghyun Kim",
       role: "Integrated M.S.-Ph.D. Program",
-      interests: "Machine Unlearning, Autonomous Driving"
+      interests: "Machine Unlearning, Autonomous Driving",
+      image: "https://lh3.googleusercontent.com/sitesv/AG8ngQUU2OQd6gAaYSyocZK0godRUpbhlsHJdvRgJF0Fc09lFCXa0-cKdtkxI3YoybJpHu7hwmmKGzsGKFOeDJgvDEFIGaMVtANBziU8qNAnT2WP_vOKNh2TuWmrguep2_fHGz0g9lmLWBuSNlpfL0ODZF1gKf7cVHn3SHiODbEWhmIaNEjoLThGJwN0LgTLO3bcrAOit3s_lwcwy2JC8ZX4l2j2v349XxSSBzOh67BbwV8=w1280"
     },
     {
       name: "Jaehyuk Yu",
       role: "Master's Program",
-      interests: "Multi-Agent System, Autonomous Driving"
+      interests: "Multi-Agent System, Autonomous Driving",
+      image: "https://lh3.googleusercontent.com/sitesv/AG8ngQWjZOXIBDP2p7Op15PbN7jFOJ9Q77I2WSggVWf1hnicTPH0HzJorKT9FrbD2tyDgbmuEqKrvG2fXcmuXIcgWdsH-Q7ZOFsVc_MLBbj8A243bc6ph3no2uA6bsK96VqxVHu8JpNeCha4R6oVQoPEhrv_StLqPxmqb_lJ0gvRacrdyW8zIzzlXBArau3rVE7O0btIbMdFVrCW2hXYUzk0aUuyZZotaW2gSfc7co_X=w1280"
     },
     {
       name: "Jinhyuk Moon",
       role: "Master's Program",
-      interests: "Quantum Machine Learning"
+      interests: "Quantum Machine Learning",
+      image: "https://lh3.googleusercontent.com/sitesv/AG8ngQUcRar4uXhK0ac7YoeICf1MZNscIc38KAOag0n1gN8kQA_P3-Qiacgd1O0iIcGrQkzM8ucAQdZWdmfYtKOJ0kBWBd4iJ6HDkcGBH8zPRXmg8OMjBQyrgNP_9kW7L52H6RlKOY1hJJCxL93JGEWMhqQxeQqx7fwulY7NMGHd3rYzd5xRJwGCCpNNTOM5qGUo4teBe6JV6dOVx1odtjO7MNyTbF2W2AHCoxl0X-hNQas=w1280"
     }
   ],
   undergraduate: [
     {
       name: "Junghoon Yang",
       role: "Undergraduate Student",
-      affiliation: "Department of Computer Science and Engineering"
+      affiliation: "Department of Computer Science and Engineering",
+      image: "https://lh3.googleusercontent.com/sitesv/AG8ngQUM-esBUmmKHBCn_ph9fIIcah1NEhN9k8_WG49GobWWkbG-0tiT4OkXFnwjAAtm1dVgPBCL74YFSkfOMEqbpzZZcIFOUWqb4TK1S3sJ7k0nDbMgk0lmGtmDhrm-GiXvC4k8rYKEeSM9OWWLyVhiD_XMo2yRGCoiIR-AwMo_-RdVbc6L6MKNY02pHTDoTEikrRB3ydpOWMeCEOzKzBMln9zNc1mHKO85ARshmJnW=w1280"
     },
     {
       name: "Kangmin Kim",
       role: "Undergraduate Student",
-      affiliation: "Department of Computer Science and Engineering"
+      affiliation: "Department of Computer Science and Engineering",
+      image: "https://lh3.googleusercontent.com/sitesv/AG8ngQWqIehJWtSD0v2BhldwLU-RXFAG6N9WspPHR_5T_wLm8J9C2Yt8zCNJcjf5zHhr0s8yv0KD-zKOTKDje9PQt63oRoKyWCcbG0N7CIa8e6c1L-DOZZ4AQQw2PIMVJw1lh8ygwVwX5ViCPYeWS2gL_yQSoldQHgeccXkXfIy1sVlO_BF0cAJ69rDK58rE35v2ev0mJVOIOhvTSMTBaVrLzhyH_bOUYADX_SIXj3JXllo=w1280"
     }
   ],
   alumni: []
